fc8cb85a936666cc1fcfb4b0ae139d8af63acdec
morley-stuff <morleyj.mail@gmail.com>
2020-01-30 12:46:32 +0100

    Adds informational component linking to basic social profiles

---
+++ src/components/Information.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Information = () => {
+  return (
+    <div>
+      <h1>Josh Morley</h1>
+      <h2>An exhaustive resource</h2>
+      <h3>Online profiles</h3>
+      <p>
+        GitHub:{" "}
+        <a href="https://github.com/morley-stuff">
+          https://github.com/morley-stuff
+        </a>
+      </p>
+      <p>
+        Facebook:{" "}
+        <a href="https://www.facebook.com/josh.morley.10">
+          https://www.facebook.com/josh.morley.10
+        </a>
+      </p>
+      <p>
+        LinkedIn:{" "}
+        <a href="https://www.linkedin.com/in/morley-stuff">
+          https://www.linkedin.com/in/morley-stuff
+        </a>
+      </p>
+      <p>
+        Twitter:{" "}
+        <a href="https://twitter.com/morley_stuff">
+          https://twitter.com/morley_stuff
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Information;
