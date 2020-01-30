c48a9e1057e301245d5007570e197f6caaf5ba36
morley-stuff <morleyj.mail@gmail.com>
2020-01-30 15:46:26 +0100

    Added introductory paragraph

--- src/components/Information.js
+++ src/components/Information.js
@@ -5,6 +5,11 @@ const Information = () => {
     <div>
       <h1>Josh Morley</h1>
       <h2>An exhaustive resource</h2>
+      <p>
+        This website was created to serve as an access point for all of my
+        public information. From online profiles, to professional and
+        educational experience, to contact methods.
+      </p>
       <h3>Online profiles</h3>
       <p>
         GitHub:{" "}
