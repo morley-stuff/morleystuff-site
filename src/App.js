8b6ef85cb4e69493d043821d2b725bba74ddb37c
morley-stuff <morleyj.mail@gmail.com>
2020-02-03 21:26:28 +0100

    Moved profile info to list of objects and created render component. Separated intro out to separate component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import Information from "./components/Information";
+import ProfileList from "./components/ProfileList";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <div className="App">
-      <Information />
+      <Intro />
+      <ProfileList />
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Josh Morley</h1>
+      <h2>An exhaustive resource</h2>
+      <p>
+        This website was created to serve as an access point for all of my
+        public information. From online profiles, to professional and
+        educational experience, to contact methods.
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
