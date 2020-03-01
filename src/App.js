d50f20da244cf57cd49b5a2de202cb198b450b2c
morley-stuff <morleyj.mail@gmail.com>
2020-03-01 21:55:03 +0100

    Basic svg image files for social accounts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./App.css";
 import ProfileList from "./components/ProfileList";
 import Intro from "./components/Intro";
+import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
+import { ReactComponent as GitHubIcon } from "./icons/github.svg";
+import { ReactComponent as LinkedInIcon } from "./icons/linkedin.svg";
+import { ReactComponent as TwitterIcon } from "./icons/twitter.svg";
 
 function App() {
   return (
     <div className="App">
       <Intro />
       <ProfileList />
+      <FacebookIcon />
+      <GitHubIcon />
+      <LinkedInIcon />
+      <TwitterIcon />
     </div>
   );
 }
