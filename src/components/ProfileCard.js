ac2dbf2efa1703a8e194b3c785ee951dafb90a68
morley-stuff <morleyj.mail@gmail.com>
2020-03-11 21:23:51 +0100

    Removed names from profile cards, and added additional profiles

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,20 +4,13 @@ import { Card } from "react-bootstrap";
 export function ProfileCard({ data }) {
   return (
     <Card className="h-100 shadow-sm bg-white rounded" href="google.com">
-      <Card.Img variant="top" src={data.image} />
       <a
         href={data.url}
         target="_blank"
         rel="noopener noreferrer"
-        class="stretched-link"
+        className="stretched-link"
       >
-        <Card.Body className="d-flex flex-column">
-          <div className="d-flex mb-2 justify-content-between">
-            <Card.Title className="mb-0 font-weight-bold">
-              {data.name}
-            </Card.Title>
-          </div>
-        </Card.Body>
+        <Card.Img variant="top" src={data.image} />
       </a>
     </Card>
   );
