71322a420ac4ec637ff0c7d976e9997a1410043b
morley-stuff <morleyj.mail@gmail.com>
2020-04-13 13:46:10 +0200

    Add collapse option to NavBar

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -4,9 +4,10 @@ import { Nav, Navbar } from "react-bootstrap";
 export function HomeNav() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand style={{ fontSize: 50, fontWeight: "bold" }}>
+      <Navbar.Brand style={{ fontSize: 40, fontWeight: "bold" }}>
         Morley Stuff
       </Navbar.Brand>
+      <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
           <Nav.Link href="/">Home</Nav.Link>
