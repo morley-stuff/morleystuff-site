477953dc184e3f932944a1a34e9dba7df3e8eab1
morley-stuff <morleyj.mail@gmail.com>
2020-04-13 12:53:13 +0200

    Testing build with default create-react-app to see if issue lies in amplify

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-import React from "react";
-import "./App.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Container, Row, Col } from "react-bootstrap";
-import profiles from "./data";
-import { HomeNav } from "./components/HomeNav";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ProfilesPage from "./components/ProfilesPage";
-import HomePage from "./components/HomePage";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <HomeNav />
-        </Col>
-      </Row>
-      <Router>
-        <Switch>
-          <Route
-            path="/profiles"
-            render={props => <ProfilesPage profiles={profiles} />}
-          />
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </Router>
-      <Row>
-        <Col>
-          <p>
-            Website
-            <br />
-            Built with{" "}
-            <a href="https://react-bootstrap.github.io/">React Bootstrap</a>
-            <br />
-            Hosted via <a href="https://aws.amazon.com/amplify/">AWS Amplify</a>
-            <br />
-            Icons from <a href="iconmonstr.com">IconMonstr</a>
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
