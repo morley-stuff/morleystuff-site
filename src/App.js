a70de58540ad4b6cbb921c041c80b999db12a148
morleystuff <morleyj.mail@gmail.com>
2021-01-02 14:12:28 +0100

    Reset to create-react-app default. This project stagnated. Starting with a clear slate.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Container, Row, Col } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import profiles from "./data";
-
-import HomePage from "./components/HomePage";
-import ProfilesPage from "./components/ProfilesPage";
-import { HomeNav } from "./components/HomeNav";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="root">
-      <HomeNav />
-      <Router>
-        <Switch>
-          <Route
-            path="/profiles"
-            render={(props) => <ProfilesPage profiles={profiles} />}
-          />
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </Router>
-      <div className="fixed-bottom bg-light">
-        <Container>
-          <p>
-            Website
-            <br />
-            Built with{" "}
-            <a href="https://react-bootstrap.github.io/">React Bootstrap</a>
-            <br />
-            Hosted via <a href="https://aws.amazon.com/amplify/">AWS Amplify</a>
-            <br />
-            Icons from <a href="iconmonstr.com">IconMonstr</a>
-          </p>
-        </Container>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
