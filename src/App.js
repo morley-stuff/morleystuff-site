d6343dadcd1b3260f30e89dbb6bc14d23bd7c2d6
morleystuff <morleyj.mail@gmail.com>
2021-01-02 17:47:39 +0100

    Add bootstrap and mirrors current site in minimal fashion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home"
+import Profiles from "./components/Profiles/Profiles";
+import MyNavBar from "./components/Nav/MyNavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNavBar />
+      <Router>
+        <Switch>
+          <Route
+            path="/profiles"
+            component={Profiles}
+          />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+      <div className="fixed-bottom bg-light">
+        <Container>Morleystuff</Container>
+      </div>
     </div>
   );
 }
