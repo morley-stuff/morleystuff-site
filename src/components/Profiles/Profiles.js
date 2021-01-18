e9d5935b6d4e07590541490f4a8b0f65dc0d00ce
morleystuff <morleyj.mail@gmail.com>
2021-01-18 19:31:43 +0100

    Add profiles from data back onto page

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import profiles from "../../data/profiles.json"
 
 class Profiles extends Component {
     render() {
         return (
             <div>
                 These are my profiles.
+            
+                {profiles.map(profile => <Container><a href={profile.url}>{profile.name}</a></Container>)}
             </div>
         );
     }
